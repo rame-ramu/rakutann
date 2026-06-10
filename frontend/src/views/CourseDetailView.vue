3d890d7c71138d1898c0583863a5af2d6ffa5999
Mizuki521 <mickey.55423c@gmail.com>
2026-06-10 07:20:45 +0000

    AGENTS.mdかいたよ

---
+++ frontend/src/views/CourseDetailView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="course-detail">
+    <h1>授業詳細</h1>
+    <p>ID: {{ $route.params.id }} の詳細を準備中...</p>
+  </div>
+</template>
---
+++ frontend/src/views/CourseListView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="course-list">
+    <h1>授業一覧</h1>
+    <p>準備中...</p>
+  </div>
+</template>
